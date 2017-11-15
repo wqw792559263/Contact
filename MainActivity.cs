8fa0fa66ccf08c2a674144fc657c048edc97753a
wqw792559263 <qq792559263@gmail.com>
2017-11-16 05:33:51 +0800

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App8
+{
+    [Activity(Label = "App8", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var contactAdapter = new ContactAdapter(this);
+            var contactsListView = FindViewById<ListView>(Resource.Id.ContactsListView);
+            contactsListView.Adapter = contactAdapter;
+
+        }
+    }
+}
+
---
+++ contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App8
+{
+    class Contact
+    {
+        public long Id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string PhotoId { get; set; }
+    }
+}
